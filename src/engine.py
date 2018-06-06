64db14a97f899f8f39b4d3f71073ac062e97519d
Qu Hao <quhxuxm_quh@163.com>
2018-06-06 09:50:03 +0800

    initialize

--- src/engine.py
+++ src/engine.py
@@ -18,12 +18,12 @@ class EngineException(Exception):
 
 @Singleton
 class Engine:
-    __logger = logging.getLogger(__name__)
     """
     The engine used to manage the flow of each step for the components
     """
 
     def __init__(self):
+        self.__logger = logging.getLogger(self.__class__.__name__)
         self.__initialize_pool()
         self.__initialize_components()
 
@@ -43,12 +43,12 @@ class Engine:
                 component_module = importlib.import_module(name)
                 component_instance = component_module.Component(short_name, configuration)
             except ModuleNotFoundError as e:
-                Engine.__logger.error("Can not found the module [%s]." % name, e)
+                self.__logger.error("Can not found the module [%s]." % name, exc_info=e)
             if component_instance is not None:
-                Engine.__logger.info("Success to find component module [%s]." % name)
+                self.__logger.info("Success to find component module [%s]." % name)
                 self.__components[short_name] = component_instance
             else:
-                Engine.__logger.info("Can not find component module [%s], the processo will use default one." % name)
+                self.__logger.info("Can not find component module [%s], the processo will use default one." % name)
                 self.__components[short_name] = DefaultComponent(short_name, configuration)
 
     def __verify_component(self, name):
@@ -61,7 +61,7 @@ class Engine:
                 self.__verify_component(name)
                 self.__components[name].p4_fetch()
             except Exception as e:
-                Engine.__logger.error("Fail to fetch the code from P4 for component [%s]." % name, e)
+                self.__logger.error("Fail to fetch the code from P4 for component [%s]." % name, exc_info=e)
 
         return self.__process_pool.submit(exec)
 
@@ -71,7 +71,7 @@ class Engine:
                 self.__verify_component(name)
                 self.__components[name].build()
             except Exception as e:
-                Engine.__logger.error("Fail to build the code for component [%s]." % name, e)
+                self.__logger.error("Fail to build the code for component [%s]." % name, exc_info=e)
 
         return self.__process_pool.submit(exec)
 
@@ -81,7 +81,7 @@ class Engine:
                 self.__verify_component(name)
                 self.__components[name].deploy()
             except Exception as e:
-                Engine.__logger.error("Fail to deploy the component [%s]." % name, e)
+                self.__logger.error("Fail to deploy the component [%s]." % name, exc_info=e)
 
         return self.__process_pool.submit(exec)
 
@@ -91,7 +91,7 @@ class Engine:
                 self.__verify_component(name)
                 self.__components[name].build_config()
             except Exception as  e:
-                Engine.__logger.error("Fail to configure the build code for component [%s]." % name, e)
+                self.__logger.error("Fail to configure the build code for component [%s]." % name, exc_info=e)
 
         return self.__process_pool.submit(exec)
 
@@ -101,7 +101,7 @@ class Engine:
                 self.__verify_component(name)
                 self.__components[name].start()
             except Exception as e:
-                Engine.__logger.error("Fail to start the component [%s]." % name, e)
+                self.__logger.error("Fail to start the component [%s]." % name, exc_info=e)
 
         return self.__process_pool.submit(exec)
 
@@ -111,7 +111,7 @@ class Engine:
                 self.__verify_component(name)
                 self.__components[name].stop()
             except Exception as e:
-                Engine.__logger.error("Fail to stop the component [%s]." % name, e)
+                self.__logger.error("Fail to stop the component [%s]." % name, exc_info=e)
 
         return self.__process_pool.submit(exec)
 
